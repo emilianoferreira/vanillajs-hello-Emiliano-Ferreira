ed7325ab134d52979fa8bea3aecee6cdb66311e0
Emiliano <82481813+emilianoferreira@users.noreply.github.com>
2024-09-21 23:54:19 -0300

    Excuse generator
    1.0

--- src/app.js
+++ src/app.js
@@ -1,11 +1,35 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+//write your code here
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+let who = ["The dog", "My grandma", "The mailman", "My bird"];
+let action = ["ate", "peed", "crushed", "broke"];
+let what = ["my homework", "my phone", "the car"];
+let when = [
+  "before the class",
+  "when I was sleeping",
+  "while I was exercising",
+  "during my lunch",
+  "while I was praying",
+];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+//Funcion que recibe un array y devuelve una posición aleatoria del mismo.
+//  Para esto, "Math.random" genera un num rnd, el cual se multiplica por el
+//largo del array, para finalmente redondearlo para abajo con .floor.
+const generarAleatorio = (arr) => arr[Math.floor(Math.random() * arr.length)];
+
+// FUNCION QUE SETEA UNA VARIABLE CON UN STRING DE LOS LLAMADOS A LA FUNCION GENERAR ALEATORIO Y LA DEVUELVE.
+const generarExcusa = () => {
+  let excusa =
+    generarAleatorio(who) +
+    " " +
+    generarAleatorio(action) +
+    " " +
+    generarAleatorio(what) +
+    " " +
+    generarAleatorio(when);
+
+  document.getElementById("excuse").innerHTML = excusa;
+
+  // let btn = document.getElementById("btnExcuse");
+  // btn.addEventListener("click", generarExcusa);
 };
+generarExcusa();
